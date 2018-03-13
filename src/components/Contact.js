161edb321dd3790fabaa2910f271637da55ddcd8
PARK,Changjun <pcj9024@gmail.com>
2018-03-13 22:20:15 +0900

    complete

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -24,6 +24,19 @@ export default class Contact extends Component {
         this.handleRemove = this.handleRemove.bind(this);
         this.handleEdit = this.handleEdit.bind(this);
     }
+    componentWillMount() {
+        const contactData  = localStorage.contactData;
+        if(contactData){
+            this.setState({
+                contactData: JSON.parse(contactData)
+            })
+        }
+    }
+    componentDidUpdate(prevProps, prevState) {
+        if (JSON.stringify(prevState.contactData) !== JSON.stringify(this.state.contactData)){
+            localStorage.contactData = JSON.stringify(this.state.contactData);
+        }
+    }
     handleCreate(contact) {
         this.setState({
             contactData: update(this.state.contactData, {$push: [contact]})
